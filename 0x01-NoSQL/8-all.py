763350f65d98dd5ebcdfc0ffd1d4506e8e41edd0
ZebbyG <zebbynjau@gmail.com>
2023-01-17 18:55:51 +0300

    Update 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-#!/usr/bin/env python3
-"""
-A Python function that lists all documents in a collection
-"""
-
-
-def list_all(mongo_collection):
-    """
-    :param mongo_collection: the pymongo collection object
-    :return: list of all documents in this collection
-    """
-    all_documents = mongo_collection.find()
-    if all_documents == 0:
-        return []
-    return all_documents
-=======
 #!/usr/bin/env python3
 """
 A Python function that lists all documents in a collection
@@ -30,4 +13,4 @@ def list_all(mongo_collection):
     if all_documents == 0:
         return []
     return all_documents
->>>>>>> 88c4115a39dff248b76182fdf4ab4acc04d5fb23
+
