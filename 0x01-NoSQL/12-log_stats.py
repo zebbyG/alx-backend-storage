952b43c3f807af0f45454e0ff6062b1d860f1afa
zebbyG <zebbynjau@gmail.com>
2023-01-17 09:47:47 +0300

    test check task 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+A Python script that provides some stats about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
+"""
+required module for task
+"""
+
+client = MongoClient()
+collection_nginx = client.logs.nginx
+num_of_docs = collection_nginx.count_documents({})
+print(f"{num_of_docs} logs")
+print("Methods:")
+method = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+for m in method:
+    count_per_method = collection_nginx.count_documents({"method": method})
+    print(f"\tmethod {m}: {count_per_method}")
+
+status_check = collection_nginx.count_documents({"method": "GET", "path": "/status"})
+print(f"{status_check} status check")
