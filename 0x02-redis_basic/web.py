3b2ac9976b475a87f97d8db979353e91b9283a57
zebbyG <zebbynjau@gmail.com>
2023-01-18 21:00:24 +0300

    retry

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,37 +1,13 @@
-#!/usr/bin/env python3
-"""Implementing an expiring web cache and tracker"""
-
-from functools import wraps
-import redis
 import requests
-from typing import Callable
-
-r = redis.Redis()
+from functools import lru_cache
 
 
-def count_requests(method: Callable) -> Callable:
-    """ Decorator for counting how many times a request
-    has been made """
-
-    @wraps(method)
-    def wrapper(url):
-        """ Wrapper for decorator functionality """
-        r.incr(f"count:{url}")
-        cached_html = r.get(f"cached:{url}")
-        if cached_html:
-            return cached_html.decode('utf-8')
-
-        html = method(url)
-        r.setex(f"cached:{url}", 10, html)
-        return html
-
-    return wrapper
-
-
-@count_requests
+@lru_cache(maxsize=None)
 def get_page(url: str) -> str:
-    """Uses the requests module to obtain the HTML
-    content of a particular URL and returns it.
-    """
-    req = requests.get(url)
-    return req.text
+    requests.get(url)
+    count = "count:" + url
+    if count in get_page.cache:
+        get_page.cache[count] += 1
+    else:
+        get_page.cache[count] = 1
+    return requests.get(url).text
